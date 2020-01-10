487655fa9f5e110acc5620b05af297e2d37b74e5
shrestharosy028 <rosy@fusemachines.com>
2020-01-10 11:48:46 +0545

    Setup components for webpack build

---
+++ src/components/dummyComponent.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class DummyComponent extends Component {
+ render() {
+   return (
+     <div>
+       Webpack configuration
+     </div>
+   )
+ }
+}
+
+export default DummyComponent
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Main from "./components/dummyComponent";
+
+ReactDOM.render(<Main/>, document.getElementById('app'));
