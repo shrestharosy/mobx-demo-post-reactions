395e1c6d9f8c4e0cdbfc2cecb12f845aaf479e2a
shrestharosy028 <rosy@fusemachines.com>
2020-01-16 14:30:07 +0545

    [STYLE] : Center image components

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,13 +2,13 @@ import React, { Component } from "react";
 import Image from "./Image";
 
 export class Dashboard extends Component {
- render() {
-   return (
-     <div>
-       <Image />
-     </div>
-   )
- }
+  render() {
+    return (
+      <div className="container-md" align="center">
+        <Image />
+      </div>
+    )
+  }
 }
 
 export default Dashboard
--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -2,8 +2,11 @@ import React from "react";
 
 export default function Image() {
     return (
-        <div className="col-4">
-            <img src="https://picsum.photos/250/300" alt="No image"></img>
+        <div className="card" style={{}}>
+            <img src="https://picsum.photos/400/400" className="card-img-top" alt="..." />>
+                <div className="card-body">
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
         </div>
     );
 }
