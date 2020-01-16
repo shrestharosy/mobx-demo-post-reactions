e52a14079b2b7da4c61ad5b2ee4b09d97a86ca7b
shrestharosy028 <rosy@fusemachines.com>
2020-01-16 15:18:51 +0545

    [STYLE] : Create and style Like and Comment buttons

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -2,10 +2,17 @@ import React from "react";
 
 export default function Image() {
     return (
-        <div className="card" style={{}}>
-            <img src="https://picsum.photos/400/400" className="card-img-top" alt="..." />>
-                <div className="card-body">
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <div className="card">
+            <img src="https://picsum.photos/400/400" className="card-img-top" alt="..." />
+            <div className="card-body">
+                <div class="row">
+                    <div class="col-sm">
+                        <button type="button" className="btn btn-light"><i className="fa fa-thumbs-o-up" />Like</button>
+                    </div>
+                    <div class="col-sm">
+                        <button type="button" className="btn btn-light"><i className="fa fa-comment-o" />Comment</button>
+                    </div>
+                </div>
             </div>
         </div>
     );
