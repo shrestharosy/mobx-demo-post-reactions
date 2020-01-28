fd76d61619c6580ae88e3f0a75707bbee779d4b7
shrestharosy028 <rosy@fusemachines.com>
2020-01-28 16:42:00 +0545

    [FEAT] : Allow user to fetch new image

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import Count from "./Count";
 import Buttons from "./Buttons";
+import imageStore from '../store/ImageStore'
+import { useObserver } from "mobx-react";
 
 export default function Card() {
     return (
-        <div className="card">
-            <img src="https://picsum.photos/400/400" className="card-img-top" alt="..." />
-            <Count />
-            <div className="card-body" >
-                <Buttons />
+        useObserver(() => (
+            <div className="card">
+                <img src={imageStore.imageUrl} className="card-img-top" alt="..." />
+                <button className="btn btn-light" onClick={() => { imageStore.fetchImage() }}>
+                    <i className="fa fa-chevron-right" />
+                </button>
+                <Count />
+                <div className="card-body" >
+                    <Buttons />
+                </div>
             </div>
-        </div>
+        ))
     );
 }
